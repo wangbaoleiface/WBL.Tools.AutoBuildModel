30bf75c7e34150aae711d11b4e9dfd95dfede326
王保雷 <wangbaolei520@sina.com>
2017-08-20 20:36:38 +0800

    提交初始代码

---
+++ WBL.Tools.AutoBuildModel/FrmAutoBuildModel.cs
@@ -0,0 +1,29 @@
+﻿//==============================================
+// 作者：王保雷
+// 时间：2017/8/20 20:23:04
+// 版本：V1.0.0.0
+// 文件名：FrmAutoBuildModel
+// 交流博客：http://www.cnblogs.com/wbl168/
+// 说明：
+//==============================================
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WBL.Tools.AutoBuildModel
+{
+    public partial class FrmAutoBuildModel : Form
+    {
+        public FrmAutoBuildModel()
+        {
+            InitializeComponent();
+        }
+    }
+}
