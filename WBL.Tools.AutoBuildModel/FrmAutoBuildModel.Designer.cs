eb254c4daecb0f855499232da7280c0c83d96afc
王保雷 <wangbaolei520@sina.com>
2017-08-20 21:43:54 +0800

    SQLServer数据库实体类的生成。

--- WBL.Tools.AutoBuildModel/FrmAutoBuildModel.Designer.cs
+++ WBL.Tools.AutoBuildModel/FrmAutoBuildModel.Designer.cs
@@ -28,11 +28,313 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmAutoBuildModel));
+            this.btnReLogin = new System.Windows.Forms.Button();
+            this.fbdShowFilePath = new System.Windows.Forms.FolderBrowserDialog();
+            this.grpOperation = new System.Windows.Forms.GroupBox();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.txtModelName = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.grpLogin = new System.Windows.Forms.GroupBox();
+            this.txtLoginName = new System.Windows.Forms.TextBox();
+            this.txtLoginPwd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDBName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSerName = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblPrompt = new System.Windows.Forms.Label();
+            this.lnklblBlog = new System.Windows.Forms.LinkLabel();
+            this.grpOperation.SuspendLayout();
+            this.grpLogin.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnReLogin
+            // 
+            this.btnReLogin.Enabled = false;
+            this.btnReLogin.Location = new System.Drawing.Point(394, 310);
+            this.btnReLogin.Name = "btnReLogin";
+            this.btnReLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnReLogin.TabIndex = 23;
+            this.btnReLogin.Text = "重新登录";
+            this.btnReLogin.UseVisualStyleBackColor = true;
+            this.btnReLogin.Click += new System.EventHandler(this.btnReLogin_Click);
+            // 
+            // fbdShowFilePath
+            // 
+            this.fbdShowFilePath.SelectedPath = "C:\\Documents and Settings\\Administrator\\桌面";
+            // 
+            // grpOperation
+            // 
+            this.grpOperation.Controls.Add(this.txtPath);
+            this.grpOperation.Controls.Add(this.txtModelName);
+            this.grpOperation.Controls.Add(this.label6);
+            this.grpOperation.Controls.Add(this.btnOK);
+            this.grpOperation.Controls.Add(this.btnShow);
+            this.grpOperation.Controls.Add(this.label7);
+            this.grpOperation.Enabled = false;
+            this.grpOperation.Location = new System.Drawing.Point(12, 152);
+            this.grpOperation.Name = "grpOperation";
+            this.grpOperation.Size = new System.Drawing.Size(548, 114);
+            this.grpOperation.TabIndex = 22;
+            this.grpOperation.TabStop = false;
+            this.grpOperation.Text = "操作";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(89, 31);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(368, 21);
+            this.txtPath.TabIndex = 6;
+            // 
+            // txtModelName
+            // 
+            this.txtModelName.Location = new System.Drawing.Point(119, 75);
+            this.txtModelName.Name = "txtModelName";
+            this.txtModelName.Size = new System.Drawing.Size(215, 21);
+            this.txtModelName.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(24, 34);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 12);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "保存路径:";
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(411, 73);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(100, 23);
+            this.btnOK.TabIndex = 9;
+            this.btnOK.Text = "生成实体类";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(463, 29);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(48, 23);
+            this.btnShow.TabIndex = 7;
+            this.btnShow.Text = "预览";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(24, 78);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(89, 12);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Model命名空间:";
+            // 
+            // grpLogin
+            // 
+            this.grpLogin.Controls.Add(this.txtLoginName);
+            this.grpLogin.Controls.Add(this.txtLoginPwd);
+            this.grpLogin.Controls.Add(this.label1);
+            this.grpLogin.Controls.Add(this.txtDBName);
+            this.grpLogin.Controls.Add(this.label2);
+            this.grpLogin.Controls.Add(this.txtSerName);
+            this.grpLogin.Controls.Add(this.btnLogin);
+            this.grpLogin.Controls.Add(this.label4);
+            this.grpLogin.Controls.Add(this.label3);
+            this.grpLogin.Location = new System.Drawing.Point(12, 12);
+            this.grpLogin.Name = "grpLogin";
+            this.grpLogin.Size = new System.Drawing.Size(544, 134);
+            this.grpLogin.TabIndex = 21;
+            this.grpLogin.TabStop = false;
+            this.grpLogin.Text = "登录信息";
+            // 
+            // txtLoginName
+            // 
+            this.txtLoginName.Location = new System.Drawing.Point(89, 65);
+            this.txtLoginName.Name = "txtLoginName";
+            this.txtLoginName.Size = new System.Drawing.Size(164, 21);
+            this.txtLoginName.TabIndex = 3;
+            // 
+            // txtLoginPwd
+            // 
+            this.txtLoginPwd.Location = new System.Drawing.Point(333, 65);
+            this.txtLoginPwd.Name = "txtLoginPwd";
+            this.txtLoginPwd.PasswordChar = '*';
+            this.txtLoginPwd.Size = new System.Drawing.Size(178, 21);
+            this.txtLoginPwd.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "服务器名:";
+            // 
+            // txtDBName
+            // 
+            this.txtDBName.Location = new System.Drawing.Point(333, 26);
+            this.txtDBName.Name = "txtDBName";
+            this.txtDBName.Size = new System.Drawing.Size(178, 21);
+            this.txtDBName.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "登录名:";
+            // 
+            // txtSerName
+            // 
+            this.txtSerName.Location = new System.Drawing.Point(89, 26);
+            this.txtSerName.Name = "txtSerName";
+            this.txtSerName.Size = new System.Drawing.Size(164, 21);
+            this.txtSerName.TabIndex = 1;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(463, 96);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(48, 23);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "登录";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(268, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 12);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "登录密码:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(268, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "数据库名:";
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.ForeColor = System.Drawing.Color.Red;
+            this.lblMsg.Location = new System.Drawing.Point(77, 281);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(245, 12);
+            this.lblMsg.TabIndex = 18;
+            this.lblMsg.Text = "您尚未登录，登录成功后才能进行生成工作！";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(475, 310);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(48, 23);
+            this.btnExit.TabIndex = 20;
+            this.btnExit.Text = "退出";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 281);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 12);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "登录提示：";
+            // 
+            // lblPrompt
+            // 
+            this.lblPrompt.AutoSize = true;
+            this.lblPrompt.Location = new System.Drawing.Point(12, 315);
+            this.lblPrompt.Name = "lblPrompt";
+            this.lblPrompt.Size = new System.Drawing.Size(65, 12);
+            this.lblPrompt.TabIndex = 24;
+            this.lblPrompt.Text = "交流博客：";
+            // 
+            // lnklblBlog
+            // 
+            this.lnklblBlog.AutoSize = true;
+            this.lnklblBlog.Location = new System.Drawing.Point(83, 315);
+            this.lnklblBlog.Name = "lnklblBlog";
+            this.lnklblBlog.Size = new System.Drawing.Size(185, 12);
+            this.lnklblBlog.TabIndex = 25;
+            this.lnklblBlog.TabStop = true;
+            this.lnklblBlog.Text = "http://www.cnblogs.com/wbl168/";
+            this.lnklblBlog.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnklblBlog_LinkClicked);
+            // 
+            // FrmAutoBuildModel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "FrmAutoBuildModel";
+            this.ClientSize = new System.Drawing.Size(573, 353);
+            this.Controls.Add(this.lnklblBlog);
+            this.Controls.Add(this.lblPrompt);
+            this.Controls.Add(this.btnReLogin);
+            this.Controls.Add(this.grpOperation);
+            this.Controls.Add(this.grpLogin);
+            this.Controls.Add(this.lblMsg);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.label5);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FrmAutoBuildModel";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "自动生成Model";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FrmAutoBuildModel_FormClosing);
+            this.grpOperation.ResumeLayout(false);
+            this.grpOperation.PerformLayout();
+            this.grpLogin.ResumeLayout(false);
+            this.grpLogin.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Button btnReLogin;
+        private System.Windows.Forms.FolderBrowserDialog fbdShowFilePath;
+        private System.Windows.Forms.GroupBox grpOperation;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.TextBox txtModelName;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.GroupBox grpLogin;
+        private System.Windows.Forms.TextBox txtLoginName;
+        private System.Windows.Forms.TextBox txtLoginPwd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDBName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSerName;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblMsg;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblPrompt;
+        private System.Windows.Forms.LinkLabel lnklblBlog;
     }
 }
--- WBL.Tools.AutoBuildModel/FrmAutoBuildModel.cs
+++ WBL.Tools.AutoBuildModel/FrmAutoBuildModel.cs
@@ -8,22 +8,278 @@
 //==============================================
 
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace WBL.Tools.AutoBuildModel
 {
+    /// <summary>
+    /// 自动生成实体类
+    /// </summary>
     public partial class FrmAutoBuildModel : Form
     {
+        /// <summary>
+        /// 链接数据库字符串
+        /// </summary>
+        private string connString = "";
+
         public FrmAutoBuildModel()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// 关闭窗体时候，退出程序
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FrmAutoBuildModel_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("您确定要退出本程序吗？", "温馨提示", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        /// <summary>
+        /// 登录服务器
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (this.txtSerName.Text.Trim() == "")
+            {
+                MessageBox.Show("服务器名称不能为空！", "温馨提示");
+                this.txtSerName.Focus();
+                return;
+            }
+            if (this.txtDBName.Text.Trim() == "")
+            {
+                MessageBox.Show("数据库名不能为空！", "温馨提示");
+                this.txtDBName.Focus();
+                return;
+            }
+            if (this.txtLoginName.Text.Trim() == "")
+            {
+                MessageBox.Show("登录名不能为空！", "温馨提示");
+                this.txtLoginName.Focus();
+                return;
+            }
+            if (this.txtLoginPwd.Text.Trim() == "")
+            {
+                MessageBox.Show("登录密码不能为空！", "温馨提示");
+                this.txtLoginPwd.Focus();
+                return;
+            }
+            connString = string.Format("Data Source={0};Initial Catalog={1};Persist Security Info=True;User ID={2};Password={3}",
+                this.txtSerName.Text.Trim(), this.txtDBName.Text.Trim(), this.txtLoginName.Text.Trim(), this.txtLoginPwd.Text.Trim());
+            SqlConnection conn = new SqlConnection(connString);
+            try
+            {
+                conn.Open();
+                conn.Close();
+                this.lblMsg.Text = "您登录成功，可以进行生成工作！";
+                this.lblMsg.ForeColor = Color.Green;
+                this.grpOperation.Enabled = true;
+                this.grpLogin.Enabled = false;
+                this.btnReLogin.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                this.lblMsg.Text = "您登录失败，不能进行生成工作！\n失败原因：" + ex.Message;
+                this.lblMsg.ForeColor = Color.Red;
+                this.grpOperation.Enabled = false;
+                this.grpLogin.Enabled = true;
+                this.btnReLogin.Enabled = false;
+            }
+        }
+
+        /// <summary>
+        /// 生成实体类
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            this.btnOK.Enabled = false;
+            if (this.txtPath.Text.Trim() == "")
+            {
+                MessageBox.Show("保存路径不能为空！", "温馨提示");
+                this.txtPath.Focus();
+                this.btnOK.Enabled = true;
+                return;
+            }
+            if (this.txtModelName.Text.Trim() == "")
+            {
+                MessageBox.Show("Model命名空间不能为空！", "温馨提示");
+                this.txtModelName.Focus();
+                this.btnOK.Enabled = true;
+                return;
+            }
+            try
+            {
+                string tableSql = "SELECT [name] FROM sys.objects WHERE [type] = N'U'";
+                DataTable tableTab = GetTable(tableSql);
+                foreach (DataRow dr in tableTab.Rows)
+                {
+                    string tableName = dr["name"].ToString();
+                    string columnNameSql = string.Format(@"SELECT C.[name] AS [name], (CASE WHEN (T.[name]='bigint' OR T.[name]='int' OR T.[name]='smallint' OR T.[name]='tinyint') THEN 'int'
+                                                           WHEN (T.[name]='char' OR T.[name]='nchar' OR T.[name]='ntext' OR T.[name]='nvarchar' OR T.[name]='text' OR T.[name]='varchar') THEN 'string'
+                                                           WHEN (T.[name]='datetime' OR T.[name]='smalldatetime') THEN 'DateTime'
+                                                           WHEN (T.[name]='decimal' OR T.[name]='money' OR T.[name]='smallmoney') THEN 'decimal'
+                                                           WHEN (T.[name]='float' OR T.[name]='real') THEN 'float' WHEN (T.[name]='numeric') THEN 'double' WHEN T.[name]='bit' THEN 'bool'
+                                                           WHEN (T.[name]='image' OR T.[name]='binary' OR T.[name]='varbinary') THEN 'byte'
+                                                           ELSE 'object' END) AS [type], ISNULL(E.value, '') AS columnDsc FROM sys.columns C 
+                                                           INNER JOIN sys.types T ON C.user_type_id = T.user_type_id
+                                                           LEFT JOIN sys.extended_properties E ON E.class = 1 AND E.major_id = C.[object_id] AND E.minor_id = C.column_id
+                                                           WHERE C.[object_id] = OBJECT_ID('{0}', N'U')", tableName);
+                    DataTable columnTab = GetTable(columnNameSql);
+                    string path = this.txtPath.Text.Trim();
+                    if (!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+                    if (tableName.IndexOf('_') != -1)
+                    {
+                        string[] tableNames = tableName.Split('_');
+                        tableName = "";
+                        for (int i = 0; i < tableNames.Length; i++)
+                        {
+                            tableName += tableNames[i].Substring(0, 1).ToUpper() + tableNames[i].Substring(1);
+                        }
+                    }
+                    else
+                    {
+                        tableName = tableName.Substring(0, 1).ToUpper() + tableName.Substring(1);
+                    }
+                    // 创建文件流
+                    FileStream fs = new FileStream(path + @"\" + tableName + ".cs", FileMode.Create);
+                    // 创建写入器
+                    StreamWriter sw = new StreamWriter(fs);
+                    // 将内容写入文件
+                    sw.WriteLine("using System;");
+                    sw.WriteLine("using System.Collections.Generic;");
+                    sw.WriteLine("using System.Text;");
+                    sw.WriteLine("");
+                    sw.WriteLine("namespace " + this.txtModelName.Text.Trim());
+                    sw.WriteLine("{");
+                    sw.WriteLine("    [Serializable]");
+                    sw.WriteLine("    public class " + tableName);
+                    sw.WriteLine("    {");
+                    sw.WriteLine("        #region 字段");
+                    sw.WriteLine("");
+                    foreach (DataRow colDr in columnTab.Rows)
+                    {
+                        sw.WriteLine("        private " + colDr["type"].ToString() + " _" +
+                            colDr["name"].ToString().Substring(0, 1).ToLower() + colDr["name"].ToString().Substring(1) + ";");
+                    }
+                    sw.WriteLine("");
+                    sw.WriteLine("        #endregion");
+                    sw.WriteLine("");
+                    sw.WriteLine("        #region 属性");
+                    sw.WriteLine("");
+                    foreach (DataRow colDr in columnTab.Rows)
+                    {
+                        string colName = "_" + colDr["name"].ToString().Substring(0, 1).ToLower() + colDr["name"].ToString().Substring(1);
+                        sw.WriteLine("        /// <summary>");
+                        sw.WriteLine("        /// " + colDr["columnDsc"].ToString());
+                        sw.WriteLine("        /// </summary>");
+                        sw.WriteLine("        public " + colDr["type"].ToString() + " " +
+                            colDr["name"].ToString().Substring(0, 1).ToUpper() + colDr["name"].ToString().Substring(1));
+                        sw.WriteLine("        {");
+                        sw.WriteLine("            get { return this." + colName + "; }");
+                        sw.WriteLine("            set { this." + colName + " = value; }");
+                        sw.WriteLine("        }");
+                        sw.WriteLine("");
+                    }
+                    sw.WriteLine("        #endregion");
+                    sw.WriteLine("    }");
+                    sw.WriteLine("}");
+                    // 关闭写入器
+                    sw.Close();
+                    // 关闭文件流
+                    fs.Close();
+                    sw.Dispose();
+                    fs.Dispose();
+                }
+                this.lblMsg.Text = "您的操作已完成，生成实体类成功！";
+                this.lblMsg.ForeColor = Color.Green;
+            }
+            catch (Exception ex)
+            {
+
+                this.lblMsg.Text = "您的操作已完成，生成实体类过程中失败！\n失败原因：" + ex.Message;
+                this.lblMsg.ForeColor = Color.Red;
+            }
+            finally
+            {
+                this.btnOK.Enabled = true;
+            }
+        }
+
+        /// <summary>
+        /// 退出
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        /// <summary>
+        /// 预览
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            this.fbdShowFilePath.Description = "请选择实体类（Model）生成的所有文件所保存的文件路径！";
+            this.fbdShowFilePath.ShowDialog();
+            this.txtPath.Text = fbdShowFilePath.SelectedPath;
+        }
+
+        /// <summary>
+        /// 重新登录
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnReLogin_Click(object sender, EventArgs e)
+        {
+            this.btnReLogin.Enabled = false;
+            this.grpLogin.Enabled = true;
+            this.grpOperation.Enabled = false;
+        }
+
+        /// <summary>
+        /// 查看博客地址
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void lnklblBlog_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("http://www.cnblogs.com/wbl168/");
+        }
+
+        /// <summary>
+        /// 获取table数据集
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <returns></returns>
+        private DataTable GetTable(string sql)
+        {
+            SqlConnection conn = new SqlConnection(connString);
+            conn.Open();
+            SqlCommand comm = new SqlCommand(sql, conn);
+            SqlDataAdapter da = new SqlDataAdapter(comm);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            return ds.Tables[0];
         }
     }
 }
